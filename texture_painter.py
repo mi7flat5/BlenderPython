bb9ac84f96669ed30be6c7fa02a3f0da8661e22d
Shane Harmon <mi7flat5@gmail.com>
2016-11-25 20:44:35 +0100

    Added CSV files for testing Modified startup to display confirmation of file opening

--- texture_painter.py
+++ texture_painter.py
@@ -5,6 +5,12 @@ Use snippets...
 import os, sys; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
 import importlib; importlib.reload(texture_painter); texture_painter.go()
 '''
+import codecs
+
+def get_backers(csv_filename):
+   
+    return codecs.open(csv_filename, 'r', 'utf-8-sig')
 
 def go():
     print("Texture Painter starting up.")
+    print(get_backers('backers_10.csv'))
