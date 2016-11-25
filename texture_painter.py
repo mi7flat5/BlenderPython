c82874b1a2f281151203301a723517096d7fcf4b
Shane Harmon <mi7flat5@gmail.com>
2016-11-25 22:10:14 +0100

    Set up CSV file Iterator\Generator

--- texture_painter.py
+++ texture_painter.py
@@ -6,11 +6,17 @@ import os, sys; sys.path.append(os.path.dirname(bpy.data.filepath)); import text
 import importlib; importlib.reload(texture_painter); texture_painter.go()
 '''
 import codecs
+import csv
 
 def get_backers(csv_filename):
-   
-    return codecs.open(csv_filename, 'r', 'utf-8-sig')
+   with codecs.open(csv_filename, 'r', 'utf-8-sig') as stream:
+       iterable = csv.reader(stream)
+       header = next(iterable)
+       for row in iterable:
+           backers = dict(zip(header,row))
+           yield backers
 
 def go():
     print("Texture Painter starting up.")
-    print(get_backers('backers_10.csv'))
+    for backer in get_backers('backers_10.csv'):
+        print(backer)
