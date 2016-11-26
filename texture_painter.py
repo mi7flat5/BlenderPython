bd6e096206a7490f637693a1a6403d8511050d0f
Shane Harmon <mi7flat5@gmail.com>
2016-11-26 20:17:39 +0100

    Simple testing with pillow render test to photo.

--- texture_painter.py
+++ texture_painter.py
@@ -1,4 +1,4 @@
-'''Renders text from a CSV file to textures
+﻿'''Renders text from a CSV file to textures
 and applies them to multiple objects.
 
 Use snippets...
@@ -7,6 +7,7 @@ import importlib; importlib.reload(texture_painter); texture_painter.go()
 '''
 import codecs
 import csv
+from PIL import Image, ImageDraw, ImageFont
 
 def get_backers(csv_filename):
    with codecs.open(csv_filename, 'r', 'utf-8-sig') as stream:
@@ -16,7 +17,30 @@ def get_backers(csv_filename):
            backers = dict(zip(header,row))
            yield backers
 
+def render_text_to_file(text_to_render):
+    burp = 1
+
+
+
 def go():
     print("Texture Painter starting up.")
     for backer in get_backers('backers_10.csv'):
         print(backer)
+    base = Image.open('stand.png').convert('RGBA')
+
+# make a blank image for the text, initialized to transparent text color
+    txt = Image.new('RGBA', base.size, (255,255,255,0))
+
+# get a font
+    fnt = ImageFont.truetype('arial.ttf', 400)
+# get a drawing context
+    d = ImageDraw.Draw(txt)
+
+# draw text, half opacity
+    d.text((100,100), "Hello", font=fnt, fill=(255,255,255,128))
+# draw text, full opacity
+    d.text((100,600), "World", font=fnt, fill=(255,255,255,255))
+
+    out = Image.alpha_composite(base, txt)
+    out.save('standWwords.png')
+    out.show()
