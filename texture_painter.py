321001feaefd6086a77051b06fadeaa63e553315
Shane Harmon <mi7flat5@gmail.com>
2016-11-27 21:11:42 +0100

    Added severel methods 	a method to duplicate an object in blender and deslect all objects. 	a method to create an offset for procedural object placement. 	Exception handleing method to ensure one and only one object is selected. 	an enumeration loop that creates a grid of objects

--- texture_painter.py
+++ texture_painter.py
@@ -5,9 +5,9 @@ Use snippets...
 import os, sys; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
 import importlib; importlib.reload(texture_painter); texture_painter.go()
 '''
-import codecs
-import csv
+import codecs, csv, os, bpy
 from PIL import Image, ImageDraw, ImageFont
+
 
 def get_backers(csv_filename):
    with codecs.open(csv_filename, 'r', 'utf-8-sig') as stream:
@@ -17,16 +17,51 @@ def get_backers(csv_filename):
            backers = dict(zip(header,row))
            yield backers
 
-def render_text_to_file(text_to_render):
-    baseimage = Image.new('RGB', (128,128))
+def render_text_to_file(text_to_render, to_filename):
+    baseimage = Image.new('RGB', (512,64))
     draw = ImageDraw.Draw(baseimage)
-    fnt = ImageFont.truetype('arial.ttf',20)
+    fnt = ImageFont.truetype('arial.ttf',50)
     draw.text((0,0),text_to_render, font = fnt, fill=(255,255,255))
-    baseimage.save('test.png')
+    baseimage.save(to_filename)
+
+def readCSV():
+    cwd = os.path.dirname(bpy.data.filepath)
+    for backer in get_backers('backers.csv'):
+        textToRender = backer.get('Name')+' '+backer.get('Country')
+        toFileName = cwd +'\\texture_cache\\'+backer.get('Number')+'.png'
+        render_text_to_file(textToRender ,toFileName)
+
+def throw_invalid_selection():
+    if len(bpy.context.selected_objects)== 0:
+        raise Exception("Please select exactly one prototype object")
+    if len(bpy.context.selected_objects) > 1:
+        raise Exception("Please select exactly one prototype object")
+
+def create_plaque(prototype,offset):
+    prototype.select = True
+    bpy.ops.object.duplicate_move(TRANSFORM_OT_translate={"value":offset})
+    new_plaque = bpy.context.selected_objects[0]
+    bpy.ops.object.select_all(action='TOGGLE')
+    return new_plaque
+
+def get_offset(num, rows, spacing):
+    x_offset = (num % rows) * spacing[0]
+    y_offst =  (num //rows) * spacing[1]
+    return (x_offset,y_offst)
 
 
 def go():
     print("Texture Painter starting up.")
-    for backer in get_backers('backers_10.csv'):
-        print(backer)
-    render_text_to_file("junk")
+    #readCSV()
+    throw_invalid_selection()
+    print("prototype object found")
+    prototype = bpy.context.selected_objects[0]
+   
+    for num, backer in enumerate(get_backers("backers_10.csv")):
+        if num== 0:
+            plaque = prototype
+        else:
+            x,y = get_offset(num,4,(2,2,0))
+            plaque = create_plaque(prototype,(x,y,0))
+        print("swapping texture to:",backer)
+
